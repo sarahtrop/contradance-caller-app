c12bf85623aeb017077733e937935578def5de6c
Sarah Trop <sctrop@gmail.com>
2022-01-04 16:43:34 -0800

    Switching to data model from classes

--- Dance Caller Cards/Dance Caller Cards/Dance_Caller_CardsApp.swift
+++ Dance Caller Cards/Dance Caller Cards/Dance_Caller_CardsApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct Dance_Caller_CardsApp: App {
+    let persistenceController = PersistenceController.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
